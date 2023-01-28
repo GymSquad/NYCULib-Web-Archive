544804ad32be2853a980cc474a8a086f0cde365d
BaBa0525 <hsunhsun0525@gmail.com>
2023-01-29 01:05:49 +0800

    :sparkles: Add record page

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,21 +1,18 @@
-interface CardProps {
-  office: string;
-  url: string;
-  updatedAt: Date;
-  clickTimes: number;
-}
+import { type ApiRecord } from "@/types/record";
+
+type CardProps = Omit<ApiRecord, "id">;
 
 export const Card = ({ office, url, updatedAt, clickTimes }: CardProps) => {
   return (
-    <>
-      <h1>{office}</h1>
+    <div className="border-b">
+      <div>{office}</div>
       <ul>
         <li>
           網站連結｜<a href={url}>{url}</a>
         </li>
-        <li>最後更新｜{updatedAt.toISOString().slice(0, 10)}</li>
+        <li>最後更新｜{updatedAt.slice(0, 10)}</li>
         <li>點擊次數｜{clickTimes}</li>
       </ul>
-    </>
+    </div>
   );
 };
