a74be74a2cb2badda16e090ffbb39491503f253d
BaBa0525 <hsunhsun0525@gmail.com>
2023-01-31 16:06:25 +0800

    🔧 Add custom colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ar: {
+          principal: "#820000",
+          bg: "#f8f6f2",
+          "text-card": "#585556",
+          collapse: "#804149",
+        },
+      },
+    },
   },
-  plugins: [require('tailwind-scrollbar-hide')],
+  plugins: [require("tailwind-scrollbar-hide")],
 };
