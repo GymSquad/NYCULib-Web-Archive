85ac67936a36200e05f540f74287d116ff090068
BaBa0525 <hsunhsun0525@gmail.com>
2023-02-17 01:08:06 +0800

    🗃️ Add data to page

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,20 +1,26 @@
-import { type ApiRecord } from "@/types/record";
+import { type FC } from "react";
+import { type CardProps } from "../types/props";
 
-type CardProps = Omit<ApiRecord, "id">;
-
-export const Card = ({ office, url, updatedAt, clickTimes }: CardProps) => {
+export const Card: FC<CardProps> = ({ name, url, updatedAt, viewCount }) => {
   return (
     <div className="border-b-[1.5px] border-gray-300 py-2 px-4">
-      <div className="py-1">{office}</div>
+      <div className="py-1">{name}</div>
       <ul className="text-sm text-ar-text-card">
-        <li>
-          網站連結｜
-          <a href={url} rel="noreferrer" target="_blank">
-            {url}
-          </a>
+        <li className="flex">
+          <label className="flex-shrink-0">網站連結｜</label>
+          <div className="min-w-0">
+            <a
+              href={url}
+              rel="noreferrer"
+              target="_blank"
+              className="block truncate"
+            >
+              {url}
+            </a>
+          </div>
         </li>
         <li>最後更新｜{updatedAt.slice(0, 10)}</li>
-        <li>點擊次數｜{clickTimes}</li>
+        <li>點擊次數｜{viewCount}</li>
       </ul>
     </div>
   );
---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export * from "./card";
+export * from "./collapse";
+export * from "./collapse-list";
+export * from "./scroll";
+export * from "./website-list";
