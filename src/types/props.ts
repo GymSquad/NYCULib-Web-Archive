e66d9ebe5f72d458c21328fb58524dbd6e8dc434
BaBa0525 <hsunhsun0525@gmail.com>
2023-02-20 22:27:44 +0800

    ♻️ Extract constant and type from page

--- src/types/props.ts
+++ src/types/props.ts
@@ -1,5 +1,15 @@
 import { type Website } from "@prisma/client";
 
-export type CardProps = Omit<Website, "id" | "officeId" | "updatedAt"> & {
+export type CardProps = Omit<Website, "officeId" | "updatedAt"> & {
   updatedAt: string;
+};
+
+export type PageProps = {
+  websites: CardProps[];
+  offices: { id: string; name: string }[];
+  departments: { id: string; name: string; urlId: string | null }[];
+  categories: { id: string; name: string; urlId: string | null }[];
+  officeId: string;
+  departmentId: string;
+  categoryId: string;
 };
