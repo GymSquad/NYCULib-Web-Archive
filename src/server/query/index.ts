03cbd849ca1b7ed948d78ab5213f681d25c8cb5b
baba <simon0525.cs09@nycu.edu.tw>
2023-05-20 03:25:39 +0800

    🔥 Remove query function

--- src/server/query/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./getCategoriesInfo";
-export * from "./getCategoryId";
-export * from "./getDepartmentId";
-export * from "./getDepartmentsInfo";
-export * from "./getOfficesInfo";
-export * from "./getWebsitesInfo";
