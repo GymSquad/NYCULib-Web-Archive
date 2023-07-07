91e36ab3091745aab9f1e286fe9b0f97bae8da68
baba <simon0525.cs09@nycu.edu.tw>
2023-07-07 17:21:32 +0800

    💄 Update timeline

--- src/components/ArchivedTimeline.tsx
+++ src/components/ArchivedTimeline.tsx
@@ -12,37 +12,6 @@ export type ArchivedTimelineProps = {
   activeDate: string;
 };
 
-// export const ArchivedTimeline = (({ times, activeDate }) => {
-//   return (
-//     <ol className="flex items-center gap-8 overflow-x-scroll scroll-smooth whitespace-nowrap border-l px-2 font-serif scrollbar-hide md:gap-16">
-//       {times.map(({ date, href }, index) => (
-//         <li key={date}>
-//           <Link
-//             replace
-//             href={href}
-//             className="flex flex-col items-center gap-2"
-//           >
-//             <span
-//               className={classNames(
-//                 "h-4 w-4 rounded-full border-2 border-ar-collapse",
-//                 {
-//                   "bg-ar-collapse": activeDate === date,
-//                   "bg-ar-bg": activeDate !== date,
-//                   "relative before:absolute before:inset-y-0 before:left-4 before:my-auto before:h-0.5 before:w-32 before:bg-ar-collapse":
-//                     index < times.length - 1,
-//                 }
-//               )}
-//             />
-//             <span className="whitespace-nowrap font-serif text-ar-collapse">
-//               {date}
-//             </span>
-//           </Link>
-//         </li>
-//       ))}
-//     </ol>
-//   );
-// }) satisfies SimplifiedFC<ArchivedTimelineProps>;
-
 export const ArchivedTimeline = (({ times, activeDate }) => {
   return (
     <ol className="relative flex items-center overflow-x-scroll scroll-smooth whitespace-nowrap font-serif scrollbar-hide ">
@@ -74,3 +43,34 @@ export const ArchivedTimeline = (({ times, activeDate }) => {
     </ol>
   );
 }) satisfies SimplifiedFC<ArchivedTimelineProps>;
+
+// export const ArchivedTimeline = (({ times, activeDate }) => {
+//   return (
+//     <ol className="flex items-center gap-8 overflow-x-scroll scroll-smooth whitespace-nowrap border-l px-2 font-serif scrollbar-hide md:gap-16">
+//       {times.map(({ date, href }, index) => (
+//         <li key={date}>
+//           <Link
+//             replace
+//             href={href}
+//             className="flex flex-col items-center gap-2"
+//           >
+//             <span
+//               className={classNames(
+//                 "h-4 w-4 rounded-full border-2 border-ar-collapse",
+//                 {
+//                   "bg-ar-collapse": activeDate === date,
+//                   "bg-ar-bg": activeDate !== date,
+//                   "relative before:absolute before:inset-y-0 before:left-4 before:my-auto before:h-0.5 before:w-32 before:bg-ar-collapse":
+//                     index < times.length - 1,
+//                 }
+//               )}
+//             />
+//             <span className="whitespace-nowrap font-serif text-ar-collapse">
+//               {date}
+//             </span>
+//           </Link>
+//         </li>
+//       ))}
+//     </ol>
+//   );
+// }) satisfies SimplifiedFC<ArchivedTimelineProps>;
